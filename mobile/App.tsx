483e198c45a762ca6839e6df4c724469ba2b358c
L3onh <leonh.engsoftware@gmail.com>
2023-07-25 17:58:08 -0300

    Aula 1 parte mobile (habits)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {
+  useFonts, 
+      Inter_400Regular,
+      Inter_600SemiBold,
+      Inter_700Bold,
+      Inter_800ExtraBold
+    } from '@expo-google-fonts/inter';
+
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontsLoded) {
+    return(
+      <Loading/>
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, // flex já é ativo por padrão, e essse 1 signinfica que ele vaia preencher toda a tela disponível
+    backgroundColor:'#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color:'#FFF'
+  }
 });
